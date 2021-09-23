835335350e2c2f9e3720f264acc8cbc0e9904770
Alex Sander <alex.sander@botcity.com.br>
2021-09-23 09:25:21 -0300

    Atualização do Template (Framework 1.3.0 e SDK 1.2.1)

--- src/main/java/dev/botcity/main/FirstBot.java
+++ src/main/java/dev/botcity/main/FirstBot.java
@@ -1,51 +1,35 @@
 package dev.botcity.main;
 
-import dev.botcity.BotChromeSDK.JavascriptCodes;
 import dev.botcity.framework.bot.DesktopBot;
+import dev.botcity.maestro_sdk.BotExecutor;
+import dev.botcity.maestro_sdk.BotMaestroSDK;
+import dev.botcity.maestro_sdk.runner.BotExecution;
+import dev.botcity.maestro_sdk.runner.RunnableAgent;
 
-public class FirstBot extends DesktopBot
-{
-	
+public class FirstBot extends DesktopBot implements RunnableAgent {
 	public FirstBot() {
-		try {
-			setResourceClassLoader(this.getClass().getClassLoader());
-			load();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		setResourceClassLoader(this.getClass().getClassLoader());
 	}
-	
-	public void action() {
-		
+
+	public void action(BotExecution execution) {
+		new BotMaestroSDK().login(execution);
 		try {
-			
 			// To open a web page
 			// browse("https://www.linkedin.com/in/lorhan/");
-			
+
 			// To exec an application (Windows)
 			// exec("calc.exe");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
 	private void notFound(String label) {
-		System.out.println("Element not found: "+label);
+		System.out.println("Element not found: " + label);
 	}
-	
-	private void load() {
-		try {
-			//addImage("", "./src/resources/");
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
+
 	public static void main(String[] args) {
-		new FirstBot().action();
+		BotExecutor.run(new FirstBot(), args);
 	}
-	
 }
-	

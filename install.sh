ce04c0569050719eb1c043c5e2be5dc93f9cbb28
Hugo Slepicka <hhslepicka@gmail.com>
2022-05-09 13:24:01 -0700

    Update maestro-sdk from 1.0 to 1.2.3.

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ mvn install:install-file -Dfile=lib/botcity-framework-core-1.3.0.jar -DgroupId=d
 mvn install:install-file -Dfile=lib/marvin-framework-2.0.jar -DgroupId=org.marvinproject -DartifactId=MarvinFramework -Dversion=2.0 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/marvin-plugins-2.0.jar -DgroupId=org.marvinproject -DartifactId=MarvinPlugins -Dversion=2.0 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/BotChromeSDK.jar -DgroupId=dev.botcity -DartifactId=BotChromeSDK -Dversion=1.0 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/maestro-sdk-1.0.jar -DgroupId=dev.botcity -DartifactId=maestro-sdk -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/maestro-sdk-1.2.3.jar -DgroupId=dev.botcity -DartifactId=maestro-sdk -Dversion=1.2.3 -Dpackaging=jar

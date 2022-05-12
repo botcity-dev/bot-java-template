a1262638e13fb951c6139eda54241f6b8406c7a2
Hugo Slepicka <hhslepicka@gmail.com>
2022-05-12 16:15:48 -0700

    Implement RunnableAgent by default.

--- src/main/java/dev/botcity/main/FirstBot.java
+++ src/main/java/dev/botcity/main/FirstBot.java
@@ -2,29 +2,31 @@ package dev.botcity.main;
 
 import dev.botcity.BotChromeSDK.JavascriptCodes;
 import dev.botcity.framework.bot.DesktopBot;
+import dev.botcity.maestro_sdk.BotExecutor;
+import dev.botcity.maestro_sdk.BotMaestroSDK;
+import dev.botcity.maestro_sdk.runner.BotExecution;
+import dev.botcity.maestro_sdk.runner.RunnableAgent;
 
-public class FirstBot extends DesktopBot
+public class FirstBot extends DesktopBot implements RunnableAgent
 {
-	
 	public FirstBot() {
 		try {
 			setResourceClassLoader(this.getClass().getClassLoader());
-			load();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void action() {
-		
+
+	@Override
+	public void action(BotExecution botExecution) {
+
 		try {
-			
 			// To open a web page
 			// browse("https://www.linkedin.com/in/lorhan/");
 			
 			// To exec an application (Windows)
 			// exec("calc.exe");
-
+ 
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -33,19 +35,9 @@ public class FirstBot extends DesktopBot
 	private void notFound(String label) {
 		System.out.println("Element not found: "+label);
 	}
-	
-	private void load() {
-		try {
-			//addImage("", "./src/resources/");
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
+
 	public static void main(String[] args) {
-		new FirstBot().action();
+		BotExecutor.run(new FirstBot(), args);
 	}
-	
 }
 	
